0211c78284dc73125213672c2e6dbe7d39393b85
ismaelrr888 <ismaelrobaina@aivo.co>
2025-02-23 23:50:09 +0200

    feat(House): add color adjustment feature for floor elements with confirmation dialog

--- src/app/components/house/HousePreview/House.tsx
+++ src/app/components/house/HousePreview/House.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
 import { useHouses } from "@/app/context/houses/HousesContext";
-import { House as HoseType } from "@/types/house";
+import { Button } from "@/app/ui/common/button";
+import {
+  ConfirmDialog,
+  DialogClose,
+  DialogTitle,
+} from "@/app/ui/common/Dialog";
+import { Floor as FloorType, House as HoseType } from "@/types/house";
+import { PaintBrushIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 interface HouseProps {
   house: HoseType;
@@ -11,8 +19,7 @@ const Window: React.FC = () => (
 
 const Floor: React.FC<{
   color: string;
-  onChangeColor: () => void;
-}> = ({ color, onChangeColor }) => {
+}> = ({ color }) => {
   return (
     <>
       <div
@@ -45,27 +52,96 @@ const Roof: React.FC = () => (
 
 export const House: React.FC<HouseProps> = ({ house }) => {
   const { handleUpdateColorFloor } = useHouses();
-  return (
-    <div key={house.id} className="flex flex-col self-end">
-      <Roof />
+  const [showDialog, setShowDialog] = useState(false);
+  const [selectedFloor, setSelectedFloor] = useState<FloorType>();
+  const [floorIndex, setFloorIndex] = useState<number>();
+  const [newColor, setNewColor] = useState<string>();
 
-      <div className="flex flex-col w-[130px] border-2 border-black">
-        {house.floors.map((floor, index) => {
-          if (index === house.floors.length - 1) {
-            return <FirstFloor key={index} color={floor.color} />;
-          } else {
-            return (
-              <Floor
-                key={index}
-                color={floor.color}
-                onChangeColor={() =>
-                  handleUpdateColorFloor(house.id, "blue", index)
-                }
-              />
+  const onShowDialog = (floor: FloorType, floorIndex: number) => {
+    setShowDialog(true);
+    setSelectedFloor(floor);
+    setFloorIndex(floorIndex);
+    setNewColor(floor.color);
+  };
+
+  const onCloseDialog = () => {
+    setShowDialog(false);
+    setSelectedFloor(undefined);
+    setFloorIndex(undefined);
+    setNewColor("");
+  };
+
+  const onUpdateFloorColor = () => {
+    if (selectedFloor && floorIndex !== undefined && newColor) {
+      handleUpdateColorFloor(house.id, newColor, floorIndex);
+      onCloseDialog();
+    }
+  };
+
+  return (
+    <>
+      <div key={house.id} className="flex flex-col self-end">
+        <Roof />
+
+        <div className="flex flex-col w-[130px] border-2 border-black">
+          {house.floors.map((floor, index) => {
+            return index === house.floors.length - 1 ? (
+              <div key={index} className="relative group">
+                <FirstFloor color={floor.color} />
+                <Button
+                  className="absolute top-0 right-[-16px] rounded-full p-1.5 opacity-0 group-hover:opacity-100 transition-opacity"
+                  aria-label="Adjust color"
+                  onClick={() => onShowDialog(floor, index)}
+                >
+                  <PaintBrushIcon className="h-3 w-3" />
+                </Button>
+              </div>
+            ) : (
+              <div key={index} className="relative group">
+                <Floor color={floor.color} />
+                <Button
+                  className="absolute top-0 right-[-16px] rounded-full p-1.5 opacity-0 group-hover:opacity-100 transition-opacity"
+                  aria-label="Adjust color"
+                  onClick={() => onShowDialog(floor, index)}
+                >
+                  <PaintBrushIcon className="h-3 w-3" />
+                </Button>
+              </div>
             );
-          }
-        })}
+          })}
+        </div>
       </div>
-    </div>
+
+      <ConfirmDialog open={showDialog} onOpenChange={setShowDialog}>
+        <DialogTitle>Update the color of the floor</DialogTitle>
+        <DialogClose>
+          <Button
+            className="absolute top-2 right-2 rounded-full p-1.5"
+            aria-label="Close"
+            onClick={onCloseDialog}
+          >
+            <XMarkIcon className="h-5 w-5" />
+          </Button>
+        </DialogClose>
+        <div className="flex flex-col justify-end gap-4 mt-4">
+          <div className="flex items-center gap-4 flex-wrap">
+            <label className="block font-semibold">Color:</label>
+            <input
+              type="color"
+              value={newColor}
+              onChange={(e) => setNewColor(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-end gap-4 mt-4">
+            <Button
+              onClick={onUpdateFloorColor}
+              className="bg-green-500 text-slate-50"
+            >
+              Save
+            </Button>
+          </div>
+        </div>
+      </ConfirmDialog>
+    </>
   );
 };
