77894378c13576ade8ded33297c364ff6eefef9f
ismaelrr888 <ismaelrobaina@aivo.co>
2025-02-24 12:40:16 +0200

    fix(HousesPreview): add space after overflow-x-auto class for improved layout consistency

--- src/app/components/house/HousePreview/HousesPreview.tsx
+++ src/app/components/house/HousePreview/HousesPreview.tsx
@@ -5,7 +5,7 @@ export const HousesPreview: React.FC = () => {
   const { houses } = useHouses();
 
   return (
-    <div className="flex gap-8 self-end overflow-x-auto">
+    <div className="flex gap-8 self-end overflow-x-auto ">
       {houses.map((house) => (
         <House key={house.id} house={house} />
       ))}
